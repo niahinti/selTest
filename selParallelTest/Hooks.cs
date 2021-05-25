d47376ae54dade42a354a0f1a113c2d4222974a7
intikz <inti.acostaylara@kzsoftworks.com>
2021-05-25 13:39:46 -0300

    test con error gecko

---
+++ selParallelTest/Hooks.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace selParallelTest
+{
+    public class Hooks : Base
+    {
+        public Hooks()
+        {
+            //initialize driver obj, from base clase, so we have just 1
+
+            Driver = new FirefoxDriver(); //at this point there was no chrome option i think   
+        }
+    }
+}
--- selParallelTest/UnitTest1.cs
+++ selParallelTest/UnitTest1.cs
@@ -1,18 +1,46 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using selParallelTest;
+using System;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        [TestFixture]
+        public class FirefoxTesting : Hooks // contract
         {
+
+            [Test]
+            public void FGoogleTest()
+            {
+                Driver.Navigate().GoToUrl("http://www.google.com");
+                Driver.FindElement(By.Name("q")).SendKeys("selenium");
+                Driver.FindElement(By.Name("btnG")).Click();
+                Assert.That(Driver.PageSource.Contains("Selenium"),Is.EqualTo(true), "text selenium doesn't exist");
+            }
+
         }
 
-        [Test]
-        public void Test1()
+        [TestFixture]
+        public class ChromeTesting : Hooks
         {
-            Assert.Pass();
+
+            [Test]
+            public void CGoogleTest()
+            {
+                Driver.Navigate().GoToUrl("http://www.google.com");
+                Driver.FindElement(By.Name("q")).SendKeys("hello world");
+                Driver.FindElement(By.Name("btnG")).Click();
+                Assert.That(Driver.PageSource.Contains("hello world"), Is.EqualTo(true), "text hello world doesn't exist");
+            }
+
         }
+
     }
 }
